e9a22f5d5bc99ff6460c976a424bc226739d6c43
v-pskum <v-pskum@TB-02-03-W-60>
2016-09-22 00:59:51 -0700

    Added Admin controller logic

--- SMSApp/SMSApp/Controllers/AdminController.cs
+++ SMSApp/SMSApp/Controllers/AdminController.cs
@@ -12,10 +12,60 @@ namespace SMSApp.Controllers
     {
         SkillManagementDal db = new SkillManagementDal();
         // GET: Admin
+
         public ActionResult Index()
         {
-            ViewBag.EmpList = db.EmpRatings.ToList<EmpRating>();
+            ViewBag.EmployeeList = db.EmpRatings.Select(x => new SelectListItem { Text = x.EmpEmail }).Distinct();
+            ViewBag.Skills = db.Skills.Select(x => new SelectListItem { Text = x.SkillName, Value = x.SkillId.ToString() }).ToList();
+            ViewBag.Ratings = db.Ratings.Select(x => new SelectListItem { Text = x.RatingId.ToString(), Value = x.RatingValue.ToString() }).ToList();
+            ViewBag.EmpList = db.EmpRatings.ToList();
             return View(ViewBag.EmpList);
         }
+
+        [ActionName("UpdateBasedOnEmployeeName")]
+        public ActionResult PopulateEmployeeName(string value)
+        {
+            List<EmpRating> employeeList = null;
+            if (value == "All")
+            {
+                employeeList = db.EmpRatings.ToList();
+            }
+            else
+            {
+                employeeList = db.EmpRatings.Where(x => x.EmpEmail == value).ToList();
+            }
+            return Json(employeeList, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost, ActionName("PopulateListBasedOnSkills")]
+        public ActionResult PopulateListBasedOnSkills(string value)
+        {
+            List<EmpRating> employeeList = null;
+            if (value == "All")
+            {
+                employeeList = db.EmpRatings.ToList();
+            }
+            else
+            {
+                employeeList = db.EmpRatings.Where(x => x.SkillName == value).ToList();
+            }
+            return Json(employeeList, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost, ActionName("PopulateListBasedOnRating")]
+        public ActionResult PopulateListBasedOnRating(string value)
+        {
+            List<EmpRating> employeeList = null;
+            if (value == "All")
+            {
+                employeeList = db.EmpRatings.ToList();
+            }
+            else
+            {
+                employeeList = db.EmpRatings.Where(x => x.Rating == value).ToList();
+            }
+            return Json(employeeList, JsonRequestBehavior.AllowGet);
+        }
     }
+
 }
